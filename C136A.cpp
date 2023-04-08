07a2e9b3ed9d38e6e55f40f2fba151ef2792bc6f
Fuzzy <fuzzphorescence@gmail.com>
2023-04-08 18:20:39 +0530

    Solved problem A

---
+++ C136A.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i ++) {
+        int no;
+        cin >> no;
+        arr[no - 1] = i + 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+}
